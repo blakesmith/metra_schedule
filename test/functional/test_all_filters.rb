1ea8cdf48aed2c899ab9a60cf96af83a1784b0f2
Blake Smith <blakesmith0@gmail.com>
2010-01-10 02:55:06 -0600

    Cleanup tests so they function properly with autotest

--- test/functional/test_all_filters.rb
+++ test/functional/test_all_filters.rb
@@ -3,20 +3,20 @@ require File.join(File.dirname(__FILE__), "../", "test_helper.rb")
 class TestLine < Test::Unit::TestCase
   include MetraSchedule::TrainData
 
-  def up_nw_stub
+  def setup
     f = File.open(File.join(File.dirname(__FILE__), '../fixture/UP_NW.html'), 'r')
     parser = MetraSchedule::Parser.new f
     parser.line = LINES[:up_nw]
-    line = Metra.new.line(:up_nw)
-    line.engines = parser.scrape
-    line
+    @line = Metra.new.line(:up_nw)
+    @line.engines = parser.scrape
+    @line
   end
 
   def test_all_filters
-    line = up_nw_stub.from(:ogilve).to(:barrington).at(Time.parse("11:29PM")).on(Date.parse("Dec 27 2009"))
-    assert_equal(1, line.trains.count)
-    line = up_nw_stub.from(:ogilve).to(:barrington).at(Time.parse("3:00AM")).on(Date.parse("Dec 27 2009"))
-    assert_equal(7, line.trains.count)
+    @line.from(:ogilve).to(:barrington).at(Time.parse("11:29PM")).on(Date.parse("Dec 27 2009"))
+    assert_equal(1, @line.trains.count)
+    @line.from(:ogilve).to(:barrington).at(Time.parse("3:00AM")).on(Date.parse("Dec 27 2009"))
+    assert_equal(7, @line.trains.count)
   end
 
 end
--- test/unit/test_parser.rb
+++ test/unit/test_parser.rb
@@ -3,12 +3,16 @@ require File.join(File.dirname(__FILE__), "../", "test_helper.rb")
 class TestLine < Test::Unit::TestCase
   include MetraSchedule::TrainData
 
-  def up_nw_stub
+  def p
     f = File.open(File.join(File.dirname(__FILE__), '../fixture/UP_NW.html'), 'r')
     parser = MetraSchedule::Parser.new f
     parser.line = LINES[:up_nw]
     parser.scrape
     parser
+  end
+
+  def up_nw_stub
+    p
   end
 
   def test_init
@@ -20,20 +24,17 @@ class TestLine < Test::Unit::TestCase
   end
 
   def test_seperate_tables
-    p = up_nw_stub
     assert_equal(2, p.tables.find_all {|t| t[:schedule] == :weekday}.count)
     assert_equal(2, p.tables.find_all {|t| t[:schedule] == :saturday}.count)
     assert_equal(2, p.tables.find_all {|t| t[:schedule] == :sunday}.count)
   end
 
   def test_sanitize
-    p = up_nw_stub
     list = [1, 2, 3]
     assert_equal(2, p.sanitize(list).count)
   end
 
   def test_train_count
-    p = up_nw_stub
     assert_equal(16, p.train_count(p.tables[0][:tables][0]))
     assert_equal(16, p.train_count(p.tables[0][:tables][1]))
     assert_equal(12, p.train_count(p.tables[1][:tables][0]))
@@ -45,13 +46,10 @@ class TestLine < Test::Unit::TestCase
   end
 
   def test_stop_count
-    p = up_nw_stub
     assert_equal(23, p.stop_count(p.tables[0][:tables][0]))
   end
 
   def test_make_stop_inbound
-    p = up_nw_stub
-
     node = p.tables[0][:tables][0].xpath("tbody[4]/tr/td[1]")[0]
     expected = MetraSchedule::Stop.new :station => :crystal_lake, :time => Time.parse("4:47AM")
     assert_equal(expected.station, p.make_stop(node, 3, :inbound).station)
@@ -59,8 +57,6 @@ class TestLine < Test::Unit::TestCase
   end
 
   def test_make_stop_outbound
-    p = up_nw_stub
-
     node = p.tables[3][:tables][0].xpath("tbody[4]/tr/td[1]")[0]
     expected = MetraSchedule::Stop.new :station => :jefferson_park, :time => Time.parse("6:13AM")
     assert_equal(expected.station, p.make_stop(node, 3, :outbound).station)
@@ -68,15 +64,11 @@ class TestLine < Test::Unit::TestCase
   end
 
   def test_make_stop_nil
-    p = up_nw_stub
-
     node = p.tables[0][:tables][0].xpath("tbody[1]/tr/td[1]")[0]
     assert_nil(p.make_stop(node, 0, :inbound))
   end
 
   def test_make_stop_am
-    p = up_nw_stub
-
     node = p.tables[0][:tables][1].xpath("tbody[18]/tr/td[5]")[0]
     expected = MetraSchedule::Stop.new :station => :norwood_park, :time => Time.parse("11:57AM")
     assert_equal(expected.station, p.make_stop(node, 17, :inbound).station)
@@ -84,8 +76,6 @@ class TestLine < Test::Unit::TestCase
   end
 
   def test_make_stop_pm
-    p = up_nw_stub
-
     node = p.tables[0][:tables][1].xpath("tbody[20]/tr/td[5]")[0]
     expected = MetraSchedule::Stop.new :station => :jefferson_park, :time => Time.parse("12:01PM")
     assert_equal(expected.station, p.make_stop(node, 19, :inbound).station)
@@ -93,8 +83,6 @@ class TestLine < Test::Unit::TestCase
   end
 
   def test_make_stop_pm_express
-    p = up_nw_stub
-
     node = p.tables[3][:tables][1].xpath("tbody[2]/tr/td[5]")[0]
     expected = MetraSchedule::Stop.new :station => :clyborn, :time => Time.parse("5:29PM")
     assert_equal(expected.station, p.make_stop(node, 1, :outbound).station)
@@ -102,15 +90,11 @@ class TestLine < Test::Unit::TestCase
   end
 
   def test_find_stops
-    p = up_nw_stub
-
     table = p.tables[0][:tables][0]
     assert_equal(19, p.find_stops(table, 1, :inbound).count)
   end
 
   def test_find_stops_express
-    p = up_nw_stub
-
     table = p.tables[0][:tables][0]
     stops = p.find_stops(table, 5, :inbound)
     assert_equal(Time.parse("6:43AM").to_f , stops[6].time.to_f)
@@ -119,13 +103,11 @@ class TestLine < Test::Unit::TestCase
   end
 
   def test_make_trains_count
-    p = up_nw_stub
     trains = p.make_trains
     assert_equal(104, p.make_trains.count)
   end
 
   def test_find_train_num
-    p = up_nw_stub
     train_602 = p.make_trains.first
     train_632 = p.make_trains[15]
 
@@ -134,7 +116,6 @@ class TestLine < Test::Unit::TestCase
   end
 
   def test_find_bike_count
-    p = up_nw_stub
     train_602 = p.make_trains.first
     train_640 = p.make_trains[18]
 
