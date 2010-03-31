610b708f31a91f78f9d505c292109f74ebdea5fa
Blake Smith <blakesmith0@gmail.com>
2010-03-31 10:29:27 -0500

    Fix time.to_today for time overflow arguments

--- test/unit/test_time_extension.rb
+++ test/unit/test_time_extension.rb
@@ -10,9 +10,42 @@ class TestTimeExtension < Test::Unit::TestCase
   end
 
   def test_to_today_midnight_inclusive
+    Timecop.freeze(2009, 8, 28, 9, 30, 45)
     sample = Time.local(2009, 5, 5, 0, 30, 45)
     today = Time.now
-    expected = Time.local(today.year, today.month, today.day+1, sample.hour, sample.min, sample.sec)
+    expected = Time.local(2009, 8, 29, 0, 30, 45)
+    assert_equal(expected, sample.to_today)
+  end
+
+  def test_to_today_on_31st
+    Timecop.freeze(2010, 3, 31, 9, 30, 45)
+    sample = Time.local(2009, 5, 5, 0, 30, 45)
+    today = Time.now
+    expected = Time.local(2010, 4, 1, 0, 30, 45)
+    assert_equal(expected, sample.to_today)
+  end
+
+  def test_to_today_on_31st_in_dec_after_midnight
+    Timecop.freeze(2010, 12, 31, 9, 30, 45)
+    sample = Time.local(2009, 5, 5, 0, 30, 45)
+    today = Time.now
+    expected = Time.local(2011, 1, 1, 0, 30, 45)
+    assert_equal(expected, sample.to_today)
+  end
+
+  def test_to_today_in_dec_after_midnight
+    Timecop.freeze(2010, 12, 21, 9, 30, 45)
+    sample = Time.local(2009, 5, 5, 0, 30, 45)
+    today = Time.now
+    expected = Time.local(2010, 12, 22, 0, 30, 45)
+    assert_equal(expected, sample.to_today)
+  end
+
+  def test_to_today_in_dec_before_midnight
+    Timecop.freeze(2010, 12, 21, 9, 30, 45)
+    sample = Time.local(2009, 5, 5, 11, 30, 45)
+    today = Time.now
+    expected = Time.local(2010, 12, 21, 11, 30, 45)
     assert_equal(expected, sample.to_today)
   end
 
