f1eb9b0560936bda99b5616bb0852c9b36e1eb4b
Blake Smith <blakesmith0@gmail.com>
2010-05-22 10:05:27 -0500

    Why on earth did I use a class level variable? I have no idea

--- test/unit/test_train.rb
+++ test/unit/test_train.rb
@@ -10,72 +10,72 @@ class TestLine < Test::Unit::TestCase
 
   def test_initialize_one_option
     assert_nothing_raised do
-      @@t = MetraSchedule::Train.new :train_num => 651
+      @t = MetraSchedule::Train.new :train_num => 651
     end
-    assert_equal(651, @@t.train_num)
+    assert_equal(651, @t.train_num)
   end
 
   def test_initialize_two_options
     assert_nothing_raised do
-      @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12
+      @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12
     end
-    assert_equal(651, @@t.train_num)
-    assert_equal(12, @@t.bike_limit)
+    assert_equal(651, @t.train_num)
+    assert_equal(12, @t.bike_limit)
   end
 
   def test_initialize_all_options
     stop = MetraSchedule::Stop.new :station => :barrington, :time => Time.now
     assert_nothing_raised do
-      @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :outbound, :stops => [stop]
+      @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :outbound, :stops => [stop]
     end
-    assert_equal(651, @@t.train_num)
-    assert_equal(12, @@t.bike_limit)
-    assert_equal(:weekday, @@t.schedule)
-    assert_equal(:outbound, @@t.direction)
-    assert_equal([stop], @@t.stops)
+    assert_equal(651, @t.train_num)
+    assert_equal(12, @t.bike_limit)
+    assert_equal(:weekday, @t.schedule)
+    assert_equal(:outbound, @t.direction)
+    assert_equal([stop], @t.stops)
   end
 
   def test_has_stop?
     stop = MetraSchedule::Stop.new :station => :barrington, :time => Time.now
-    @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :outbound, :stops => [stop]
-    assert_equal(true, @@t.has_stop?(:barrington))
-    assert_equal(false, @@t.has_stop?(:arlington_park))
+    @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :outbound, :stops => [stop]
+    assert_equal(true, @t.has_stop?(:barrington))
+    assert_equal(false, @t.has_stop?(:arlington_park))
   end
 
   def test_in_time?
     stop1 = MetraSchedule::Stop.new :station => :barrington, :time => Time.parse("12:30PM")
     stop2 = MetraSchedule::Stop.new :station => :arlington_heights, :time => Time.parse("12:40PM")
-    @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
-    assert_equal(true, @@t.in_time?(:arlington_heights, Time.parse("12:35PM")))
-    assert_equal(false, @@t.in_time?(:barrington, Time.parse("12:35PM")))
+    @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
+    assert_equal(true, @t.in_time?(:arlington_heights, Time.parse("12:35PM")))
+    assert_equal(false, @t.in_time?(:barrington, Time.parse("12:35PM")))
   end
 
   def test_in_time_midnight_inclusive
     stop1 = MetraSchedule::Stop.new :station => :arlington_heights, :time => Time.parse("12:30AM")
     stop2 = MetraSchedule::Stop.new :station => :cary, :time => Time.parse("2:45AM")
     stop3 = MetraSchedule::Stop.new :station => :harvard, :time => Time.parse("4:00AM")
-    @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2, stop3]
-    assert_equal(true, @@t.in_time?(:arlington_heights, Time.parse("11:45PM")))
-    assert_equal(true, @@t.in_time?(:cary, Time.parse("11:45PM")))
-    assert_equal(false, @@t.in_time?(:harvard, Time.parse("11:45PM")))
+    @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2, stop3]
+    assert_equal(true, @t.in_time?(:arlington_heights, Time.parse("11:45PM")))
+    assert_equal(true, @t.in_time?(:cary, Time.parse("11:45PM")))
+    assert_equal(false, @t.in_time?(:harvard, Time.parse("11:45PM")))
   end
 
   def test_in_time_same_time_next_day
     stop1 = MetraSchedule::Stop.new :station => :barrington, :time => Time.parse("12:30PM")
     stop2 = MetraSchedule::Stop.new :station => :arlington_heights, :time => Time.parse("12:40PM")
-    @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
+    @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
     tomorrow = Time.parse("12:35PM") + (60 * 60 * 24)
-    assert_equal(true, @@t.in_time?(:arlington_heights, tomorrow))
-    assert_equal(false, @@t.in_time?(:barrington, tomorrow))
+    assert_equal(true, @t.in_time?(:arlington_heights, tomorrow))
+    assert_equal(false, @t.in_time?(:barrington, tomorrow))
   end
 
   def test_departure_and_arrival
     stop1 = MetraSchedule::Stop.new :station => :barrington, :time => Time.parse("12:30")
     stop2 = MetraSchedule::Stop.new :station => :arlington_heights, :time => Time.parse("12:40")
-    @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
+    @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
 
     l = Metra.new.line(:up_nw)
-    l.engines = [@@t]
+    l.engines = [@t]
     train = l.trains.first
     assert_equal({:departure => Time.parse("12:30"), :arrival => Time.parse("12:40")}, train.departure_and_arrival(:barrington, :arlington_heights))
   end
@@ -84,10 +84,10 @@ class TestLine < Test::Unit::TestCase
     Timecop.freeze(2010, 4, 29) do
       stop1 = MetraSchedule::Stop.new :station => :barrington, :time => Time.parse("12:30PM")
       stop2 = MetraSchedule::Stop.new :station => :arlington_heights, :time => Time.parse("12:40PM")
-      @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
+      @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
 
       l = Metra.new.line(:up_nw)
-      l.engines = [@@t]
+      l.engines = [@t]
       l.on(Date.today + 1)
       train = l.trains.first
       assert_not_nil train
@@ -99,10 +99,10 @@ class TestLine < Test::Unit::TestCase
   def test_my_departure_and_my_arrival
     stop1 = MetraSchedule::Stop.new :station => :barrington, :time => Time.parse("12:30")
     stop2 = MetraSchedule::Stop.new :station => :arlington_heights, :time => Time.parse("12:40")
-    @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
+    @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
 
     l = Metra.new.line(:up_nw).from(:barrington).to(:arlington_heights)
-    l.engines = [@@t]
+    l.engines = [@t]
     train = l.trains.first
     assert_equal(Time.parse("12:30"), train.my_departure)
     assert_equal(Time.parse("12:40"), train.my_arrival)
@@ -112,12 +112,12 @@ class TestLine < Test::Unit::TestCase
     Timecop.freeze(2010, 1, 5) do
       stop1 = MetraSchedule::Stop.new :station => :barrington, :time => Time.parse("12:30")
       stop2 = MetraSchedule::Stop.new :station => :arlington_heights, :time => Time.parse("12:40")
-      @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
+      @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
     end
 
     Timecop.freeze(2010, 1, 6) do
       l = Metra.new.line(:up_nw).from(:barrington).to(:arlington_heights)
-      l.engines = [@@t]
+      l.engines = [@t]
       train = l.trains.first
       assert_equal(Time.parse("12:30"), train.my_departure)
       assert_equal(Time.parse("12:40"), train.my_arrival)
@@ -127,10 +127,10 @@ class TestLine < Test::Unit::TestCase
   def test_my_departure_and_my_arrival_with_no_start_or_destination
     stop1 = MetraSchedule::Stop.new :station => :barrington, :time => Time.parse("12:30")
     stop2 = MetraSchedule::Stop.new :station => :arlington_heights, :time => Time.parse("12:40")
-    @@t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
+    @t = MetraSchedule::Train.new :train_num => 651, :bike_limit => 12, :schedule => :weekday, :direction => :inbound, :stops => [stop1, stop2]
 
     l = Metra.new.line(:up_nw)
-    l.engines = [@@t]
+    l.engines = [@t]
     train = l.trains.first
     assert_equal(nil, train.my_departure)
     assert_equal(nil, train.my_arrival)
