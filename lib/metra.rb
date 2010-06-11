088257734aa2474af1105fb83c5ccf9d7dac9c48
Blake Smith <blakesmith0@gmail.com>
2010-06-11 14:12:18 -0500

    InstanceMethods

--- lib/metra.rb
+++ lib/metra.rb
@@ -2,7 +2,7 @@ Bundler.setup
 
 require 'date'
 require 'time'
-require File.join(File.dirname(__FILE__), 'metra', 'classmethods')
+require File.join(File.dirname(__FILE__), 'metra', 'instancemethods')
 require File.join(File.dirname(__FILE__), 'metra', 'line')
 require File.join(File.dirname(__FILE__), 'metra', 'train')
 require File.join(File.dirname(__FILE__), 'metra', 'stop')
